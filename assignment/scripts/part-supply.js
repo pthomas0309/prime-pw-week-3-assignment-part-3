ff3c7a295ed30abea7ecbaaffee2ea34a2f024ee
pthomas0309 <preston.thomas355@gmail.com>
2021-04-14 15:45:46 -0500

    defined array supplyChanges and set it to 3, 5, -6, 0, 7, 11. logged array

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -8,7 +8,8 @@ console.log('1. Number of partsNeeded:', partsNeeded);
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
-console.log('2. Array of supplyChanges:');
+let supplyChanges = [ '3', '5', '-6', '0', '7', '11' ]
+console.log('2. Array of supplyChanges:', supplyChanges );
 
 // 3. Console log the value of 'supplyChanges' at index 2
 console.log('3. Item at index 2 is:');
